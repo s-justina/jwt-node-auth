0e3106591d9456f21477d95abd505d13954514cd
s-justina <jus.sobcz@gmail.com>
2021-02-04 21:28:28 +0100

    add login controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,27 +1,76 @@
 const mongoose = require("mongoose");
 const User = require("../models/users");
+const bcrypt = require("bcrypt");
 
 const connUri = process.env.MONGO_LOCAL_CONN_URL;
 
 module.exports = {
   add: (req, res) => {
-    mongoose.connect(connUri, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+    mongoose.connect(
+      connUri,
+      { useNewUrlParser: true, useUnifiedTopology: true },
+      (err) => {
+        let result = {};
+        let status = 201;
+        if (!err) {
+          const { name, password } = req.body;
+          const user = new User({ name, password }); // document = instance of a model
+          // TODO: We can hash the password here before we insert instead of in the model
+          user.save((err, user) => {
+            if (!err) {
+              result.status = status;
+              result.result = user;
+            } else {
+              status = 500;
+              result.status = status;
+              result.error = err;
+            }
+            res.status(status).send(result);
+          });
+        } else {
+          status = 500;
+          result.status = status;
+          result.error = err;
+          res.status(status).send(result);
+        }
+      }
+    );
+  },
+  login: (req, res) => {
+    const { name, password } = req.body;
+
+    mongoose.connect(connUri, { useNewUrlParser: true }, (err) => {
       let result = {};
-      let status = 201;
+      let status = 200;
       if (!err) {
-        const { name, password } = req.body;
-        const user = new User({ name, password }); // document = instance of a model
-        // TODO: We can hash the password here before we insert instead of in the model
-        user.save((err, user) => {
-          if (!err) {
-            result.status = status;
-            result.result = user;
+        User.findOne({ name }, (err, user) => {
+          if (!err && user) {
+            // We could compare passwords in our model instead of below
+            bcrypt
+              .compare(password, user.password)
+              .then((match) => {
+                if (match) {
+                  result.status = status;
+                  result.result = user;
+                } else {
+                  status = 401;
+                  result.status = status;
+                  result.error = "Authentication error";
+                }
+                res.status(status).send(result);
+              })
+              .catch((err) => {
+                status = 500;
+                result.status = status;
+                result.error = err;
+                res.status(status).send(result);
+              });
           } else {
-            status = 500;
+            status = 404;
             result.status = status;
             result.error = err;
+            res.status(status).send(result);
           }
-          res.status(status).send(result);
         });
       } else {
         status = 500;
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
-const controller = require('../controllers/users');
+const controller = require("../controllers/users");
 
 module.exports = (router) => {
-    router.route('/users')
-        .post(controller.add);
+  router.route("/users").post(controller.add);
+
+  router.route("/login").post(controller.login);
 };
