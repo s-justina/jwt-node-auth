145326f3e3c85f03d1051e2627b17b8d12a9d20b
s-justina <jus.sobcz@gmail.com>
2021-02-04 23:14:30 +0100

    add authentication via token; let use GET/users only for admin

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,11 @@
-const controller = require("../controllers/users");
+const controller = require('../controllers/users');
+const validateToken = require('../utils').validateToken;
 
 module.exports = (router) => {
-  router.route("/users").post(controller.add);
+  router.route('/users')
+      .post(controller.add)
+      .get(validateToken, controller.getAll); // This route is now protected
 
-  router.route("/login").post(controller.login);
+  router.route('/login')
+      .post(controller.login);
 };
